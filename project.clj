64407590fad1be34729b994a3ba3b976547f2db3
Caio Rulli <caiorulli@gmail.com>
2019-10-20 22:59:11 -0300

    Successfully added hiccup

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.7.1"]
-                 [stasis "2.5.0"]]
+                 [stasis "2.5.0"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler caiorulli.github.io/app}
   :repl-options {:init-ns caiorulli.github.io}
--- src/caiorulli/github/io.clj
+++ src/caiorulli/github/io.clj
@@ -1,15 +1,42 @@
 (ns caiorulli.github.io
   (:require [stasis.core :as stasis]
+            [hiccup.core :refer [html]]
             [caiorulli.github.cleaner :as cleaner]))
 
+(def home-page
+  (html [:html
+         [:head
+          [:meta {:charset "utf-8"}]
+          [:meta {:name    "viewport"
+                  :content "width=device-width, initial-scale=1"}]
+          [:title "caiorulli.github.io"]]
+         [:body
+          [:h1 "Meu objetivo é a conquista!"]
+          [:iframe {:width           "560"
+                    :height          "315"
+                    :src             "https://www.youtube.com/embed/80SSXCgdCuU"
+                    :frameborder     "0"
+                    :allow           "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    :allowfullscreen "true"}]]]))
+
 (defn- get-pages []
-  {"/index.html" "<h1>Hello world!</h1>"})
+  {"/index.html" home-page})
 
 (def app
   (stasis/serve-pages get-pages))
 
-(def target-dir "./caiorulli.github.io")
+(def export-dir "./caiorulli.github.io")
 
-(defn export []
-  (cleaner/empty-git-directory! target-dir)
-  (stasis/export-pages (get-pages) target-dir))
+(defn- load-export-dir []
+  (stasis/slurp-directory export-dir #"\.[^.]+$"))
+
+(defn export
+  "Export the entire site as flat files to the export-dir."
+  []
+  (let [old-files (load-export-dir)]
+    (cleaner/empty-git-directory! export-dir)
+    (stasis/export-pages (get-pages) export-dir)
+    (println)
+    (println "Export complete:")
+    (stasis/report-differences old-files (load-export-dir))
+    (println)))
