b9529075f682bd87c3781571a0ec8450509b92a2
Caio Rulli <caiorulli@gmail.com>
2019-10-21 23:25:19 -0300

    Removing unused files

--- test/caiorulli/github/io_test.clj
+++
@@ -1,7 +0,0 @@
-(ns caiorulli.github.io-test
-  (:require [clojure.test :refer :all]
-            [caiorulli.github.io :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
