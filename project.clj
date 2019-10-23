c36b6a52ec06552df5d30e6166ef0cd4536e2c81
Caio Rulli <caiorulli@gmail.com>
2019-10-22 23:05:00 -0300

    Added firebase config, upgraded clojure

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
                  [reagent "0.8.1"]]
 
